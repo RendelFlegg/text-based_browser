43c35923e128c23ab79cfca278b921756958f42e
RendelFlegg <rendelflegg@ya.ru>
2022-05-11 11:57:33 +0300

    Completed Stage 4: Implemented actual browsing

--- Text-Based Browser/task/browser.py
+++ Text-Based Browser/task/browser.py
@@ -1,16 +1,24 @@
 import argparse
 import os
+import requests
 from collections import deque
 
 
-def save_cash(page_address):
-    with open(f'{directory}/{page_address[:-4]}', 'w', encoding='utf-8') as f:
-        f.write(url_dictionary[page_address])
+def save_cash(url, content):
+    url = url.strip('https://')[:url.rfind('.')]
+    with open(f'{directory}/{url}', 'w', encoding='utf-8') as f:
+        f.write(content)
 
 
 def load_cash(page_address):
-    with open(f'{directory}/{page_address}', 'r') as f:
+    with open(f'{directory}/{page_address}', 'r', encoding='utf-8') as f:
         print(f.read())
+
+
+def url_check(page_address):
+    if not page_address.startswith('https://'):
+        page_address = 'https://' + page_address
+    return page_address
 
 
 parser = argparse.ArgumentParser(description='Text browser')
@@ -21,58 +29,23 @@ directory = args.dir
 if not os.access(directory, os.F_OK):
     os.mkdir(directory)
 
-nytimes_com = '''
-This New Liquid Is Magnetic, and Mesmerizing
-
-Scientists have created “soft” magnets that can flow 
-and change shape, and that could be a boon to medicine 
-and robotics. (Source: New York Times)
-
-
-Most Wikipedia Profiles Are of Men. This Scientist Is Changing That.
-
-Jessica Wade has added nearly 700 Wikipedia biographies for
- important female and minority scientists in less than two 
- years.
-
-'''
-
-bloomberg_com = '''
-The Space Race: From Apollo 11 to Elon Musk
-
-It's 50 years since the world was gripped by historic images
- of Apollo 11, and Neil Armstrong -- the first man to walk 
- on the moon. It was the height of the Cold War, and the charts
- were filled with David Bowie's Space Oddity, and Creedence's 
- Bad Moon Rising. The world is a very different place than 
- it was 5 decades ago. But how has the space race changed since
- the summer of '69? (Source: Bloomberg)
-
-
-Twitter CEO Jack Dorsey Gives Talk at Apple Headquarters
-
-Twitter and Square Chief Executive Officer Jack Dorsey 
- addressed Apple Inc. employees at the iPhone maker’s headquarters
- Tuesday, a signal of the strong ties between the Silicon Valley giants.
-'''
-
-url_dictionary = {'nytimes.com': nytimes_com, 'bloomberg.com': bloomberg_com}
 history = deque()
 
 address = input()
-
 while address.lower() != 'exit':
-    if '.' not in address and address in os.listdir(directory) and address.lower() != 'back':
-        load_cash(address)
-        history.append(address)
-    elif address in url_dictionary:
-        print(url_dictionary[address])
-        save_cash(address)
-        history.append(address[:-4])
-    elif address.lower() == 'back':
+    if address.lower() == 'back':
         if history:
             history.pop()
             load_cash(history.pop())
+    elif '.' not in address:
+        if address in os.listdir(directory):
+            load_cash(address)
+            history.append(address)
+        else:
+            print('Error: Incorrect URL')
     else:
-        print('Error: Incorrect URL')
+        r = requests.get(url_check(address))
+        print(r.text)
+        save_cash(address, r.text)
+        history.append(address[:address.rfind('.')])
     address = input()
